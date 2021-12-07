cd851c2fc134448d7a488d8745daea0be55da030
jkn1708 <jkn17082@naver.com>
2021-12-07 23:35:40 +0900

    12/07_java

---
+++ chapter2/src/binary/BinaryTest2.java
@@ -0,0 +1,12 @@
+package binary;
+
+public class BinaryTest2 {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = -5;
+		int sum = num1 + num2;
+		System.out.println(sum);
+	}
+
+}
