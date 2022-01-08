fd48cca10e533100f41bf842baf0ed47379f3613
jkn1708 <jkn17082@naver.com>
2022-01-08 13:41:01 +0900

    part 10~20

--- First/src/first/Operation.java
+++ First/src/first/Operation.java
@@ -4,6 +4,22 @@ public class Operation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//삼항 연산자
+		
+		int k = 10 > 20 ? 100:200;
+		System.out.println(k);
+		
+		//전위 연산자     k1을 증가시키고 k2 에 대입
+		
+		int k1= 100;
+		int k2= ++k1;
+		System.out.println(k1+ " " + k2);
+		
+		//후위 연산자    m1을 먼저 m2에 대입하고 m2를 증가
+		
+		int m1 = 100;
+		int m2 = m1++;
+		System.out.println(m1 + " " +m2);
 		
 
 	}
---
+++ First/src/first/Test1.java
@@ -0,0 +1,14 @@
+package first;
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//숫자 입력 받기
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt(); 
+		System.out.println(number);
+	}
+
+}
